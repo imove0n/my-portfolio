b3eba6b2755b91dd39fd42d5b7c57762d320d580
Laurence De Guzman <laurence.deguzman@tup.edu.ph>
2025-10-02 09:14:44 +0800

    Improve loading experience by adding visibility and opacity transitions to prevent flash of unstyled content

--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,28 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <style dangerouslySetInnerHTML={{
+          __html: `
+            html {
+              visibility: hidden !important;
+              opacity: 0 !important;
+            }
+            html.loaded {
+              visibility: visible !important;
+              opacity: 1 !important;
+              transition: opacity 0.3s ease;
+            }
+          `
+        }} />
       </Head>
       <body>
         <Main />
         <NextScript />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            document.documentElement.classList.add('loaded');
+          `
+        }} />
       </body>
     </Html>
   )
