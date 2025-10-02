49c85570f1b063c8fd35d86d54b6ba53e749bc4f
Laurence De Guzman <laurence.deguzman@tup.edu.ph>
2025-10-02 13:08:59 +0800

    Refactor loading handling and styles to enhance user experience and prevent flash of unstyled content

--- pages/_app.js
+++ pages/_app.js
@@ -1,48 +1,9 @@
 import '../styles/globals.css'
 import { Analytics } from "@vercel/analytics/next"
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-  const [isTransitioning, setIsTransitioning] = useState(false)
-
-  useEffect(() => {
-    const handleStart = () => setIsTransitioning(true)
-    const handleComplete = () => setIsTransitioning(false)
-
-    router.events.on('routeChangeStart', handleStart)
-    router.events.on('routeChangeComplete', handleComplete)
-    router.events.on('routeChangeError', handleComplete)
-
-    return () => {
-      router.events.off('routeChangeStart', handleStart)
-      router.events.off('routeChangeComplete', handleComplete)
-      router.events.off('routeChangeError', handleComplete)
-    }
-  }, [router])
-
   return (
     <>
-      {isTransitioning && (
-        <div style={{
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          background: '#0f172a',
-          zIndex: 99999,
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          color: '#0ea5e9',
-          fontSize: '1.2rem',
-          fontFamily: 'Inter, sans-serif'
-        }}>
-          Loading...
-        </div>
-      )}
       <Component {...pageProps} />
       <Analytics />
     </>
