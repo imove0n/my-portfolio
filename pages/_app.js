f28d8eb1caddf8ce8103681dec090115dc2edc27
Laurence De Guzman <laurence.deguzman@tup.edu.ph>
2025-10-09 13:09:57 +0800

    Add Vercel Speed Insights integration; enhance performance monitoring in the application

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
@@ -76,6 +77,7 @@ function MyApp({ Component, pageProps }) {
 
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   )
 }
